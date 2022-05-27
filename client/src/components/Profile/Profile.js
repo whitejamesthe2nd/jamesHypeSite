1e9b0adfeae3440cf2ce464129786bf6860fa09f
James White <whitejamesthe2nd@gmail.com>
2022-05-26 21:06:59 -0400

    lots to do

---
+++ client/src/components/Profile/Profile.js
@@ -0,0 +1,13 @@
+
+import profilePic from './profPic.webp';
+
+
+export default function Profile(){
+    return (
+        <>
+        <div className={'profile'}>
+            <img className={'profile'} src={profilePic} alt='high marc'/>
+            </div>
+        </>
+    )
+}
